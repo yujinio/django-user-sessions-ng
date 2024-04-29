0e6b7ca50e0c499bb0ec1225513c7a95af2fa61a
yujinio <root@yujin.io>
2024-04-29 20:58:58 +0200

    fix error where the device was not assosiated with any variable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.4
+
+- Fixed error when local variable 'device' is not associated with a value
+
 # 0.1.3
 
 - Fixed `SessionMiddleware` import in `middleware`
--- django_user_sessions_ng/middleware.py
+++ django_user_sessions_ng/middleware.py
@@ -10,7 +10,5 @@ class SessionMiddleware(DjangoSessionMiddleware):
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
         ip = get_client_ip(request)[0]
         user_agent = request.headers.get("User-Agent", None)
-        if user_agent:
-            device = str(user_agents.parse(user_agent))
-
+        device = str(user_agents.parse(user_agent)) if user_agent else None
         request.session = self.SessionStore(ip=ip, device=device, session_key=session_key)
