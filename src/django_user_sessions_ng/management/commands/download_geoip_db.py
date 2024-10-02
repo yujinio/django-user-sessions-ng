2598100e9a0c61ccb3378dac9578e12bb99961c1
yujinio <root@yujin.io>
2024-10-02 18:48:17 +0200

    ADD path argument to download cmd

--- src/django_user_sessions_ng/management/commands/download_geoip_db.py
+++ src/django_user_sessions_ng/management/commands/download_geoip_db.py
@@ -21,56 +21,55 @@ class Command(BaseCommand):
             default=DEFAULT_MAXMIND_GEOIP_DOWNLOAD_BASE_URL,
             help=f"Base URL for downloading GeoIP2 database files. The default is {DEFAULT_MAXMIND_GEOIP_DOWNLOAD_BASE_URL}.",
         )
+        parser.add_argument(
+            "-p",
+            "--path",
+            default=getattr(settings, "GEOIP_PATH", None),
+            help="Path to store GeoIP2 database files. The default is GEOIP_PATH in settings.py.",
+        )
 
     def handle(
         self,
         maxmind_license_key: str,
         maxmind_geoip_download_base_url: str,
+        path: str | None,
         *args,
         **options,
     ) -> None:
-        if not (db_path := getattr(settings, "GEOIP_PATH", None)):
+        if not path:
             return self.stderr.write("GEOIP_PATH is not set in settings.py. Aborting.")
 
-        db_path = Path(db_path)
+        dbpath = Path(path)
 
-        if not db_path.exists():
-            db_path.mkdir(parents=True)
+        if not dbpath.exists():
+            dbpath.mkdir(parents=True)
 
-        for basename, url in (
-            (
-                "GeoLite2-City.tar.gz",
-                self.get_download_url(
-                    maxmind_geoip_download_base_url,
-                    maxmind_license_key,
-                    "GeoLite2-City",
-                ),
-            ),
-            (
-                "GeoLite2-Country.tar.gz",
-                self.get_download_url(
-                    maxmind_geoip_download_base_url,
-                    maxmind_license_key,
-                    "GeoLite2-Country",
-                ),
-            ),
-        ):
-            fp = db_path / basename
-            self.stdout.write(f"Downloading {basename} to {fp}...")
-            urllib.request.urlretrieve(url, str(fp))  # nosec
-            self.stdout.write(f"Downloaded {fp}. Extracting...")
-            self.extract_tar(db_path, fp)
-            self.stdout.write(f"Extracted {fp}. Clealing...")
-            fp.unlink()
-            self.stdout.write("Cleaned.")
+        for basename in ("GeoLite2-City", "GeoLite2-Country"):
+            self.download_geoip_db(dbpath, maxmind_license_key, maxmind_geoip_download_base_url, basename)
 
-    @staticmethod
-    def get_download_url(maxmind_geoip_download_base_url: str, maxmind_license_key: str, edition: str) -> str:
+        self.stdout.write("GeoIP2 database files have been downloaded and extracted successfully.")
+
+    def download_geoip_db(
+        self, dbpath: Path, maxmind_license_key: str, maxmind_geoip_download_base_url: str, edition: str
+    ) -> None:
+        basename: str = f"{edition}.tar.gz"
+        download_url: str = self.build_download_url(maxmind_geoip_download_base_url, maxmind_license_key, edition)
+
+        fp = dbpath / basename
+        self.stdout.write(f"Downloading {basename} to {fp}...")
+        urllib.request.urlretrieve(download_url, str(fp))  # nosec
+        self.stdout.write(f"Downloaded {fp}. Extracting...")
+        self.extract_tar(dbpath, fp)
+        self.stdout.write(f"Extracted {fp}. Cleaning...")
+        fp.unlink()
+        self.stdout.write(f"Cleaned {fp}.")
+
+    def build_download_url(self, maxmind_geoip_download_base_url: str, maxmind_license_key: str, edition: str) -> str:
         return f"{maxmind_geoip_download_base_url}?{urlencode({'edition_id': edition, 'license_key': maxmind_license_key, 'suffix': 'tar.gz'})}"
 
-    def extract_tar(self, db_path: Path, tar_path: Path) -> None:
+    def extract_tar(self, dbpath: Path, tar_path: Path) -> None:
         with tarfile.open(tar_path) as tarball:
             for tarinfo in tarball:
                 if tarinfo.name.endswith(".mmdb"):
                     tarinfo.name = os.path.basename(tarinfo.name)
-                    tarball.extract(tarinfo, path=str(db_path))
+                    tarball.extract(tarinfo, path=str(dbpath))
